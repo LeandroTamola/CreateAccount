f56547cbeb61a3a2ee14af0e7326dc6d6bed1b5f
Leandro Tamola <leatamola@gmail.com>
2023-05-12 23:36:31 -0300

    configured tsconfig and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  ignorePatterns: ['node_modules/*', '!.prettierrc.js'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-class-static-block',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
